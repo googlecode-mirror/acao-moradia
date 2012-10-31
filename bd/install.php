1daaac353b41a3e70003c80b10c5cf447e73ebcf
lpfw82 <lpfw82@gmail.com>
2012-10-31 03:52:28 +0000

    - Alteração na chave primária de Endereço: ANTES era o CEP agora é o CEP + LOGRADOURO + NUMERO. - Alteração no CEP de endereço: ANTES era VARCHAR(8) agora é VARCHAR(9) para acomodar os números e o hifen. - Alteração no sexo na tabela pessoa: AGORA É 'M' ou 'F' - Alteração data_término na tabela curso deve ser >= data_início - Criação de Triggers: 	Em CURSO_HAS_PESSOA define AUTOMATICAMENTE a situação da matrícula da pessoa 	Em Curso(nome,pre_requisitos,dias_semana) para converter para maiúsculo AUTOMATICAMENTE 	Em Programa(nome) para converter para maiúsculo AUTOMATICAMENTE 	Em Bairro(nome) para converter para maiúsculo AUTOMATICAMENTE 	Em Cidade(nome,estado) para converter para maiúsculo AUTOMATICAMENTE 	Em Endereco(logradouro) para converter para maiúsculo AUTOMATICAMENTE 	Em Pessoa(nome,sexo) para converter para maiúsculo AUTOMATICAMENTE

--- bd/install.php
+++ bd/install.php
@@ -1,4 +1,4 @@
-<?/*INSTALA O BANCO DE DADOS*/
+<?php /*INSTALA O BANCO DE DADOS*/
 
 ########DADOS PARA CONEXÃO COM O MYSQL
 $server = "localhost";
@@ -22,11 +22,11 @@ mysql_query('DROP DATABASE IF EXISTS acao_moradia');
 mysql_query('CREATE DATABASE acao_moradia');				
 mysql_query('USE acao_moradia');
 
-########CRIA AS TABELAS
+#---------------------------------------------------CRIA AS TABELAS------------------------------------------------------
 mysql_query("
 CREATE TABLE IF NOT EXISTS `login` (
-  `usuario` VARCHAR(50) PRIMARY KEY,  
-  `senha` VARCHAR(40) NOT NULL, 
+  `usuario` VARCHAR(50) PRIMARY KEY,
+  `senha` VARCHAR(40) NOT NULL, 	
   `nivel` TEXT NOT NULL CHECK(nivel IN ('ATENDENTE','ADMIN'))
 ) ENGINE=InnoDB;
 ") or die(mysql_error());
@@ -36,13 +36,13 @@ echo "Tabela login Instalada com sucesso<br>";
 mysql_query("
 CREATE TABLE IF NOT EXISTS `curso` (
   `id` INT AUTO_INCREMENT NOT NULL,
-  `nome` VARCHAR(100) NOT NULL,
+  `nome` VARCHAR(100) NOT NULL,			#upper
   `vagas` SMALLINT,
   `data_inicio` DATE NOT NULL,
   `carga_horaria` FLOAT4,
-  `pre_requisitos` TEXT,
-  `dias_semana` TEXT,
-  `data_termino` DATE NOT NULL,
+  `pre_requisitos` TEXT,				#upper
+  `dias_semana` TEXT,					#upper
+  `data_termino` DATE NOT NULL CHECK(`data_termino` >= `data_inicio`),
   CONSTRAINT `curso_pk` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 ") or die(mysql_error());
@@ -52,7 +52,7 @@ echo "Tabela curso Instalada com sucesso<br>";
 mysql_query("
 CREATE TABLE IF NOT EXISTS `programa` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
+  `nome` VARCHAR(100) NOT NULL,			#upper
   CONSTRAINT `programa_pk` PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 ") or die(mysql_error());
@@ -61,7 +61,7 @@ echo "Tabela programa Instalada com sucesso<br>";
 
 mysql_query("
 CREATE TABLE IF NOT EXISTS `bairro` (
-  `nome` VARCHAR(100) PRIMARY KEY
+  `nome` VARCHAR(100) PRIMARY KEY		#upper
 ) ENGINE=InnoDB;
 ") or die(mysql_error());
 
@@ -69,8 +69,8 @@ echo "Tabela bairro Instalada com sucesso<br>";
 
 mysql_query("
 CREATE TABLE IF NOT EXISTS `cidade` (
-  `nome` VARCHAR(100) NOT NULL,
-  `estado` VARCHAR(2) NOT NULL,
+  `nome` VARCHAR(100) NOT NULL,			#upper
+  `estado` VARCHAR(2) NOT NULL,			#upper
   CONSTRAINT `cidade_pk` PRIMARY KEY (`nome`, `estado`)
 ) ENGINE=InnoDB;
 ") or die(mysql_error()); 
@@ -79,15 +79,15 @@ echo "Tabela cidade Instalada com sucesso<br>";
 
 mysql_query("
 CREATE TABLE IF NOT EXISTS `endereco` (
-  `cep` VARCHAR(8),
-  `logradouro` VARCHAR(100) NOT NULL,  
-  `numero` INT UNSIGNED NOT NULL,
-  `bairro_nome` VARCHAR(100) NOT NULL,
-  `cidade_nome` VARCHAR(100) NOT NULL,
-  `cidade_estado` VARCHAR(2) NOT NULL,
-  CONSTRAINT `endereco_pk` PRIMARY KEY (`cep`),
-  CONSTRAINT `bairro_nome_fk` FOREIGN KEY (`bairro_nome`) REFERENCES bairro(`nome`) ON UPDATE CASCADE,
-  CONSTRAINT `cidade_fk` FOREIGN KEY (`cidade_nome`, `cidade_estado`) REFERENCES cidade(`nome`, `estado`) ON UPDATE CASCADE
+  `cep` VARCHAR(9),
+  `logradouro` VARCHAR(100) NOT NULL,  #upper
+  `numero` INT(11) NOT NULL,
+  `bairro` VARCHAR(100) NOT NULL,	#essas três são chaves estrangeiras, portanto, já são upper
+  `cidade` VARCHAR(100) NOT NULL,  
+  `estado` VARCHAR(2) NOT NULL,  
+  CONSTRAINT `endereco_pk` PRIMARY KEY (`cep`, `logradouro`, `numero`),
+  CONSTRAINT `bairro_nome_fk` FOREIGN KEY (`bairro`) REFERENCES bairro(`nome`) ON UPDATE CASCADE,
+  CONSTRAINT `cidade_fk` FOREIGN KEY (`cidade`, `estado`) REFERENCES cidade(`nome`, `estado`) ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 ") or die(mysql_error());
 
@@ -97,17 +97,19 @@ mysql_query("
 CREATE TABLE IF NOT EXISTS `pessoa` (
   `id` INT AUTO_INCREMENT NOT NULL,
   `cpf` VARCHAR(14),
-  `nome` VARCHAR(100) NOT NULL,
+  `nome` VARCHAR(100) NOT NULL,			#upper
   `rg` VARCHAR(45),
-  `sexo` CHAR,
+  `sexo` CHAR CHECK(`sexo`IN('M','F')),	#upper
   `data_nascimento` DATE,
   `data_cadastro` DATE,
-  `data_saida` DATE,  
-  `endereco_cep` VARCHAR(8) NOT NULL,
+  `data_saida` DATE,    
   `id_conjuge` INT,
+  `cep` VARCHAR(9) NOT NULL,
+  `logradouro` VARCHAR(100) NOT NULL ,	
+  `numero` INT(11) NOT NULL ,
   CONSTRAINT `pessoa_pk` PRIMARY KEY (`id`),
   CONSTRAINT `conjuge_fk` FOREIGN KEY (`id_conjuge`) REFERENCES pessoa(`id`),
-  CONSTRAINT `endereco_cep_fk` FOREIGN KEY (`endereco_cep`) REFERENCES endereco(`cep`)
+  CONSTRAINT `endereco_cep_fk` FOREIGN KEY (`cep`, `logradouro`, `numero`) REFERENCES endereco(`cep`, `logradouro`, `numero`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 ") or die(mysql_error());
 
@@ -125,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `telefone` (
 echo "Tabela telefone Instalada com sucesso<br>";
 
 mysql_query("
-CREATE TABLE IF NOT EXISTS `pesssoa_has_programa` (
+CREATE TABLE IF NOT EXISTS `pessoa_has_programa` (
   `id_pessoa` INT NOT NULL,
   `id_programa` INT NOT NULL,
   CONSTRAINT `pessoa_has_programa_pk` PRIMARY KEY (`id_pessoa`, `id_programa`),
@@ -140,7 +142,7 @@ mysql_query("
 CREATE TABLE IF NOT EXISTS `pessoa_has_pessoa` (
   `id_pessoa` INT NOT NULL,
   `id_parente` INT NOT NULL,
-  `parentesco` VARCHAR(45) NOT NULL,
+  `parentesco` VARCHAR(45) NOT NULL,	#upper
   CONSTRAINT `pessoa_has_pessoa_pk` PRIMARY KEY(`id_pessoa`, `id_parente`),
   CONSTRAINT `pessoa_has_pessoa_id_pessoa_fk` FOREIGN KEY(`id_pessoa`) REFERENCES pessoa(`id`),
   CONSTRAINT `pessoa_has_pessoa_id_parente_fk` FOREIGN KEY(`id_parente`) REFERENCES pessoa(`id`)
@@ -153,7 +155,7 @@ mysql_query("
 CREATE TABLE IF NOT EXISTS `curso_has_pessoa` (
   `id_curso` INT NOT NULL,
   `id_pessoa` INT NOT NULL,  
-  `situacao_matricula` TINYTEXT NOT NULL,
+  `situacao_matricula` TINYTEXT NOT NULL,		#upper
   CONSTRAINT `curso_has_pessoa_pk` PRIMARY KEY(`id_pessoa`, `id_curso`),
   CONSTRAINT `curso_has_pessoa_id_pessoa_fk` FOREIGN KEY(`id_pessoa`) REFERENCES pessoa(`id`),
   CONSTRAINT `curso_has_pessoa_id_curso_fk` FOREIGN KEY(`id_curso`) REFERENCES curso(`id`)
@@ -161,5 +163,220 @@ CREATE TABLE IF NOT EXISTS `curso_has_pessoa` (
 ") or die(mysql_error());
 
 echo "Tabela curso_has_pessoa Instalada com sucesso<br>";
+
+#--------------------------------------------------------TRIGGERS-------------------------------------------------------
+
+#------------LETRAS Maiúsculas em `curso` -------------
+echo "<br>";
+#INSERT
+mysql_query("
+CREATE TRIGGER insere_curso BEFORE INSERT ON curso
+  FOR EACH ROW BEGIN    
+	set NEW.nome = upper(NEW.nome);
+	set NEW.pre_requisitos = upper(NEW.pre_requisitos);
+	set NEW.dias_semana = upper(NEW.dias_semana);
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao inserir curso Instalada com sucesso<br>";
+
+#UPDATE
+mysql_query("
+CREATE TRIGGER atualiza_curso BEFORE UPDATE ON curso
+  FOR EACH ROW BEGIN    
+	set NEW.nome = upper(NEW.nome);
+	set NEW.pre_requisitos = upper(NEW.pre_requisitos);
+	set NEW.dias_semana = upper(NEW.dias_semana);
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao atualizar curso Instalada com sucesso<br>";
+
+
+#-----------LETRAS Maiúsculas em `PROGRAMA` -----------
+#INSERT
+mysql_query("
+CREATE TRIGGER insere_programa BEFORE INSERT ON programa
+  FOR EACH ROW BEGIN    
+	set NEW.nome = upper(NEW.nome);
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao inserir programa Instalada com sucesso<br>";
+
+#UPDATE
+mysql_query("
+CREATE TRIGGER atualiza_programa BEFORE UPDATE ON programa
+  FOR EACH ROW BEGIN    
+	set NEW.nome = upper(NEW.nome);
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao atualizar programa Instalada com sucesso<br>";
+
+
+#----------LETRAS Maiúsculas em `BAIRRO` -------------
+#INSERT
+mysql_query("
+CREATE TRIGGER insere_bairro BEFORE INSERT ON bairro
+  FOR EACH ROW BEGIN    
+	set NEW.nome = upper(NEW.nome);
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao inserir bairro Instalada com sucesso<br>";
+
+#UPDATE
+mysql_query("
+CREATE TRIGGER atualiza_bairro BEFORE UPDATE ON bairro
+  FOR EACH ROW BEGIN    
+	set NEW.nome = upper(NEW.nome);
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao atualizar bairro Instalada com sucesso<br>";
+
+
+#---------LETRAS Maiúsculas em `CIDADE` --------------
+#INSERT
+mysql_query("
+CREATE TRIGGER insere_cidade BEFORE INSERT ON cidade
+  FOR EACH ROW BEGIN    
+	set NEW.nome = upper(NEW.nome);
+	set NEW.estado = upper(NEW.estado);
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao inserir cidade Instalada com sucesso<br>";
+
+#UPDATE
+mysql_query("
+CREATE TRIGGER atualiza_cidade BEFORE UPDATE ON cidade
+  FOR EACH ROW BEGIN    
+	set NEW.nome = upper(NEW.nome);
+	set NEW.estado = upper(NEW.estado);
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao atualizar cidade Instalada com sucesso<br>";
+
+
+#---------LETRAS Maiúsculas em `ENDEREÇO` ------------
+#INSERT
+mysql_query("
+CREATE TRIGGER insere_endereco BEFORE INSERT ON endereco
+  FOR EACH ROW BEGIN    
+	set NEW.logradouro = upper(NEW.logradouro);	
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao inserir endereço Instalada com sucesso<br>";
+
+#UPDATE
+mysql_query("
+CREATE TRIGGER atualiza_endereco BEFORE UPDATE ON endereco
+  FOR EACH ROW BEGIN    
+	set NEW.logradouro = upper(NEW.logradouro);	
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao atualizar endereço Instalada com sucesso<br>";
+
+
+#-----------LETRAS Maiúsculas em `PESSOA` ------------
+#INSERT
+mysql_query("
+CREATE TRIGGER insere_pessoa BEFORE INSERT ON pessoa
+  FOR EACH ROW BEGIN    
+	set NEW.nome = upper(NEW.nome);	
+	set NEW.sexo = upper(NEW.sexo);
+	set NEW.data_cadastro = date(now());
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao inserir pessoa Instalada com sucesso<br>";
+
+#UPDATE
+mysql_query("
+CREATE TRIGGER atualiza_pessoa BEFORE UPDATE ON pessoa
+  FOR EACH ROW BEGIN    
+	set NEW.nome = upper(NEW.nome);	
+	set NEW.sexo = upper(NEW.sexo);		
+  END;
+") or die(mysql_error());
+
+echo "Trigger ao atualizar pessoa Instalada com sucesso<br>";
+
+
+#-----------DEFINE SITUAÇÃO_MATRICULA EM`CURSO_HAS_PESSOA` ------------
+#INSERT
+mysql_query("
+CREATE TRIGGER insere_aluno_em_curso BEFORE INSERT ON curso_has_pessoa
+  FOR EACH ROW BEGIN	
+    DECLARE qtd_vagas_ocupadas, qtd_vagas_curso INT;
+
+    SELECT COUNT(*) FROM curso_has_pessoa WHERE id_curso = NEW.id_curso INTO qtd_vagas_ocupadas;
+    SELECT COUNT(*) FROM curso WHERE id = NEW.id_curso INTO qtd_vagas_curso;
+    
+    IF(qtd_vagas_curso > qtd_vagas_ocupadas) THEN
+         set NEW.situacao_matricula = 'MATRICULADO';
+    ELSE
+         set NEW.situacao_matricula = 'LISTA_ESPERA';
+           
+    END IF;
+  END;
+") or die(mysql_error());
+
+echo "Trigger para definir situação_matricuça em curso_has_pessoa Instalada com sucesso<br>";
+
+
+#---------------------------------------------------POPULAR O BANCO-------------------------------------------------------
+echo "<br>";
+#CURSO
+mysql_query("insert into curso(nome,vagas,data_inicio,carga_horaria,pre_requisitos,dias_semana,data_termino) values
+('informática',2,'2013-01-10',45,'','terça e quinta', '2013-03-10')") or die(mysql_error());
+echo "Tabela curso populada com sucesso<br>";
+
+#PROGRAMA
+mysql_query("insert into programa(nome) values ('criança feliz'),('formação infantil')") or die(mysql_error());
+echo "Tabela programa populada com sucesso<br>";
+
+#BAIRRO
+mysql_query("insert into bairro(nome) values ('MOrUMBI'),('ACLImAÇÃO'),('santa mônica')") or die(mysql_error());
+echo "Tabela bairro populada com sucesso<br>";
+
+#CIDADE
+mysql_query("insert into cidade(nome, estado) values ('UBERLÂNDIA','MG')") or die(mysql_error());
+echo "Tabela cidade populada com sucesso<br>";
+
+#ENDEREÇO
+mysql_query("insert into endereco(cep,logradouro,numero,bairro,cidade,estado) values ('38408-100','AVENIDA: joão naves de ávila', 2121,'santa mônica','uberlândia','MG')") or die(mysql_error());
+echo "Tabela cidade populada com sucesso<br>";
+
+#PESSOA
+mysql_query("insert into pessoa(cpf,nome,rg,sexo,data_nascimento,id_conjuge,cep,logradouro,numero) 
+values ('101.101.101-00','João da silva', 2121,'m','1970-10-31',null,'38408-100','AVENIDA: joão naves de ávila', 2121)") or die(mysql_error());
+echo "Tabela cidade populada com sucesso<br>";
+
+#TELEFONE
+mysql_query("insert into telefone(id_pessoa, numero) 
+values (1,'(34) 9690-1101')") or die(mysql_error());
+echo "Tabela telefone populada com sucesso<br>";
+
+#PESSOA_HAS_PROGRAMA
+mysql_query("insert into pessoa_has_programa(id_pessoa, id_programa) values (1,1)") or die(mysql_error());
+echo "Tabela pesssoa_has_programa Instalada com sucesso<br>";
+
+#CURSO_HAS_PESSOA
+mysql_query("insert into curso_has_pessoa(id_curso, id_pessoa) values (1,1)") or die(mysql_error());
+echo "Tabela curso_has_pessoa Instalada com sucesso<br>";
+
+
+#PESSOA_HAS_PESSOA
+
+
+
+echo "<br>BANCO INSTALADO COM SUCESSO<br>";
+
 
 ?>
