1cd043f8b8bf2c3b46b6b747a556c925d7972f9b
renato.rsufu <renato.rsufu@gmail.com>
2012-11-21 18:09:19 +0000

    Importando arquivos.

---
+++ acao/bairroDAO.php
@@ -0,0 +1,40 @@
+<?php
+    class BairroDAO{
+        
+        private $_ins= "INSERT INTO  bairro (`nome`) VALUES";
+        private $_rem= "DELETE FROM bairro WHERE";
+        private $_alt= "UPDATE bairro set";
+        private $_sel= "SELECT * FROM bairro";
+        
+         public function testeInsert($_res){
+            if($_res != TRUE)
+                echo 'falaha na operação';  
+        }
+        
+        public function cadastraBairro($b){       
+            $this->_ins.= " ('$b')"; 
+            $_res= mysql_query($this->_ins);
+            if($_res != TRUE)
+                echo 'falaha na operação B'; 
+        }             
+        
+        public function removeBairro($nome){            
+            $this->_rem.= " usuario= $user AND senha= $pass AND nivel= $level";
+            $_res= mysql_query($this->rem);
+            this.testeInsert($_res);
+        }   
+        
+        public function buscaBairro($bairro){
+            $this->_sel.= " WHERE nome= '$bairro'";
+            $res= mysql_query($this->_sel);
+            if($res === FALSE){
+                echo "bairro desconhecido";
+                return null;
+            }else{
+                $arived= mysql_fetch_assoc($res);
+                return $arived;
+            }            
+        }      
+    }
+?>
+
---
+++ acao/controleCadastroPessoa.php
@@ -0,0 +1,78 @@
+<?php
+include_once 'controleData.php';
+echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
+
+    include_once 'modelo.php';
+    include_once 'bairroDAO.php';
+    include_once 'pessoaDAO.php';
+    include_once 'dBConection.php';
+    
+    $nome= $_GET['nome'];
+    $cpf= $_GET['cpf'];
+    $rg= $_GET['rg'];
+    $sexo= $_GET['sexo'];
+    
+    if($sexo === "Masculino")
+        $sexo= 'M';
+        elseif ($sexo === "Feminino") 
+            $sexo= 'F';   
+
+    
+    $dataNascimento= $_GET['dataNascimento'];
+    $dataNascimento= Date::toMySqlDate($dataNascimento);
+
+    //pegando a data de cadastro do sistema
+    //date_default_timezone_set('America/Sao_Paulo');
+    //$dataCadastro= date('m/d/Y');
+    
+    $telefone= $_GET['telefone'];
+    
+    $logradouro= $_GET['logradouro'];
+    $numero= $_GET['numero'];
+    $cidadeNome= $_GET['cidade'];
+    $bairroNome= $_GET['bairro'];
+    $cidadeEstado= $_GET['estado'];
+    $cep= $_GET['cep'];
+    $idConjuge = "NULL";
+
+    
+    
+    //$idPessoa= 01;
+    
+    DataBase::createConection();
+    
+
+
+    /*
+     procura se ja existe uma familia com este endereço, se jah, retorna tds os atributos dela p preencher os campos...
+     * se naum, da a opção de criar a familia
+     */
+
+    
+    //enquanto endereços
+    $bairroClass= new Bairro($bairroNome);//persistir bairro
+    $bDao= new BairroDAO();
+    $vet= $bDao->buscaBairro($bairroNome);
+    if(sizeof($vet)>0){
+    
+        //echo $vet['nome'];   
+   
+        $cidadeClass= new Cidade($cidadeNome, $cidadeEstado);//persistir cidade
+        //enquanto telefones
+        //$telefonrClass= new Telefone($idPessoa, $numero);//persistir telefone
+        
+        //$endereco= new Endereco($cep, $logradouro, $numero, $bairroClass, $cidadeClass, $cidadeEstado);//persistir endereço
+   
+        //fazer uma validação de cpf...    
+    
+        //$pessoa= new Pessoa($idPessoa, $cpf, $nome, $rg, $sexo, $dataNascimento, $dataCadastro, $dataSaida, $cep, null);
+        $pDAO= new PessoaDAO();
+        $tep= $pDAO->cadastraPessoa( $cpf, $nome, $rg, $sexo, $dataNascimento, $idConjuge, $cep, $logradouro, $numero);
+        if($tep === true){
+            header("Location: cCadastraCursos.php");
+        }
+        
+    }
+    
+    
+    ?>
