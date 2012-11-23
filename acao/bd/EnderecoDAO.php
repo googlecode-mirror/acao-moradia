f9c09a45587e55d216fd01a52e2ece784b4e5e28
lpfw82 <lpfw82@gmail.com>
2012-11-23 18:28:19 +0000

    Organizando os arquivos  em pastas

---
+++ acao/bd/EnderecoDAO.php
@@ -0,0 +1,35 @@
+<?php
+    class EnderecoDAO{
+        /*
+        private $_ins= "INSERT INTO  endereco (`cep`, `logradouro`, `numero`, `bairro`, `cidade`, `estado`) VALUES";
+        private $_rem= "DELETE FROM endereco WHERE";
+        private $_alt= "UPDATE endereco set";
+        private $_sel= "SELECT * FROM endereco";                 
+        
+        public function cadastraEndereco($cep, $logradouro, $numero, $bairro, $cidade, $estado){       
+            $this->_ins.= " ('$b')";
+            $_res= mysql_query($this->_ins);
+            if($_res != TRUE)
+                echo 'falaha na operação B'; 
+        }             
+        
+        public function removeBairro($nome){            
+            $this->_rem.= " usuario= $user AND senha= $pass AND nivel= $level";
+            $_res= mysql_query($this->rem);
+            this.testeInsert($_res);
+        }   
+        
+        public function buscaBairro($bairro){
+            $this->_sel.= " WHERE nome= '$bairro'";
+            $res= mysql_query($this->_sel);
+            if($res === FALSE){
+                echo "bairro desconhecido";
+                return null;
+            }else{
+                $arived= mysql_fetch_assoc($res);
+                return $arived;
+            }            
+        } */     
+    }
+?>
+
