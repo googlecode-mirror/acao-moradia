33c11b2da482d4ffa7c47a4de226e114dfd220ae
lpfw82@gmail.com <lpfw82@gmail.com@a9a1e803-4b22-2b6a-56cc-01784704d63b>
2012-12-02 17:59:32 +0000

    Não sei pq, mas no commit 33 não foi alterado este arquivo.

---
+++ trunk/acao/visao/vcCidades.php
@@ -0,0 +1,14 @@
+<?php
+    $idestado = $_GET['estado'];
+
+    include_once '../bd/DBConnection.php';
+    DataBase::createConection();
+
+    $result = mysql_query("SELECT * FROM cidade WHERE cod_estado = ".$idestado);
+
+    while($row = mysql_fetch_array($result) ){
+         echo "<option value='".$row['cod_cidade']."'>".$row['nome']."</option>";
+
+    }
+
+?>
