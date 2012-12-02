e5929934c125aa549f07d5be8ff8ec86f0a1db5d
renato.rsufu <renato.rsufu@gmail.com>
2012-12-02 16:20:30 +0000

    funções de buscade familia todas funcionando (por id, numero, titular, logradouro)

--- acao/controle/montaTabela.php
+++ acao/controle/montaTabela.php
@@ -24,7 +24,7 @@ $query= $_REQUEST['query'];
 $fD= new FamiliaDAO;
     $pD= new PessoaDAO;
 
-if(!$query){   
+if(!$query){//busca vazia retorno irrelevante 
     $resultado= $fD->buscaFamilia();
     $html  .= "<table border= 1 width=500px>";    
     $html .= "<th>id</th>";
@@ -57,6 +57,18 @@ elseif (is_numeric($query)) { //provavel q a pesquisa seja por ID
         $html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
         $html .= '</tr>';      
     }
+    
+    //if(mysql_num_rows($resultadoId)){
+    $resultadoNum= $fD->buscaFamiliaByNumero($query);
+    while ($a= mysql_fetch_assoc($resultadoNum)){
+        $html .= '<tr>';
+        $html .= '<td>'.$a['id_familia'].'</td>';
+        $html .= '<td>'.$pD->buscaPessoabyFamilia($a['id_familia']).'</td>';
+        $html .= '<td>'.$a['logradouro'].' - '.$a['numero'].'</td>';
+        $html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
+        $html .= '</tr>'; 
+    }
+    
     //concatena com resultados menos relevantes, pois pode ser que a primeira resposta naum seja importante
     $resultadoId2= $fD->buscaFamiliaExceptId($query);
     while ($a= mysql_fetch_assoc($resultadoId2)){        
@@ -70,6 +82,7 @@ elseif (is_numeric($query)) { //provavel q a pesquisa seja por ID
 }
 
  else {//a pesquisa é texto, pode ser um endereço ou titular
+     //criar contador p fazer uma busca * caso os dois selects falhem
    $tamanhoPesquisa= strlen($query);
    $radical= substr($query, 1, $tamanhoPesquisa-2);
    $resultado3= $pD->buscaPessoaTitular($query);
@@ -95,6 +108,16 @@ elseif (is_numeric($query)) { //provavel q a pesquisa seja por ID
         $html .= '<td>'.$a['logradouro'].' - '.$a['numero'].'</td>';
         $html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
         $html .= '</tr>';  
+    }
+    //mostrar resultados menos relevantes
+    $resultado= $fD->buscaFamiliaExceptLogradouro($query);
+    while ($a= mysql_fetch_assoc($resultado)){        
+        $html .= '<tr>';
+        $html .= '<td>'.$a['id_familia'].'</td>';
+        $html .= '<td>'.$pD->buscaPessoabyFamilia($a['id_familia']).'</td>';
+        $html .= '<td>'.$a['logradouro'].' - '.$a['numero'].'</td>';
+        $html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
+        $html .= '</tr>';        
     }
     
 }
