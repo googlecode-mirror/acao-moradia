eeaaeb16260d3268fd15147fdd68c8807a8e2849
brunocesar.bsi@gmail.com <brunocesar.bsi@gmail.com@a9a1e803-4b22-2b6a-56cc-01784704d63b>
2012-12-03 18:48:51 +0000

    Estou mexendo com o cadastro de Funcionários

--- trunk/acao/controle/cCadastraFuncionario.php
+++ trunk/acao/controle/cCadastraFuncionario.php
@@ -5,13 +5,18 @@
     include_once 'cFuncoes.php';
     echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
 
-    include_once 'modelo/Modelo.php';
-    include_once 'bd/BDConnection.php';
+    include_once '../modelo/Modelo.php';
+    include_once '../bd/LoginDAO.php';
     
-    $usuario= $_GET['usuario'];
-    $senha= $_GET['senha'];
-    $nivel= $_GET['nivel'];
-       
+    
+    $usuario= $_POST['usuario'];
+    $senha= $_POST['senha'];
+    $nivel= $_POST['nivel'];
+    
+    $login = new Login($usuario, $senha, $nivel);
+    $loginDAO = new LoginDAO();
+    $loginDAO->cadastraLogin($login);
+    
        // corrigir isso aqui depois 
        // $pDAO = new pDAO();
        // $tep= $pDAO->cadastraFuncionario( $usuario, $senha, $nivel);
