449dde87c224437007d0a8d031dbc50cf6e6cbdc
rafael.vmendes2 <rafael.vmendes2@gmail.com>
2012-12-04 14:54:22 +0000

    Mudança de nomes de variáveis (padronização)

--- acao/bd/CursoDAO.php
+++ acao/bd/CursoDAO.php
@@ -4,7 +4,7 @@
     class CursoDAO{
         
         private $_ins= "INSERT INTO `curso`(`nome`, `vagas`, `data_inicio`, `carga_horaria`, `pre_requisitos`, `dias_semana`, `data_termino`) VALUES";
-        //private $_rem= "DELETE FROM curso WHERE";
+        private $_rem= "DELETE FROM curso WHERE `nome`= ";
         //private $_alt= "UPDATE curso set";
         //private $_sel= "SELECT * FROM curso";
         
@@ -24,12 +24,22 @@
         public function cadastraCurso2($curso) {
             return $this->cadastraCurso($curso->getNome(), $curso->getVagas(), $curso->getDataInicio(), $curso->getCargaHoraria(), $curso->getPreRequisitos(), $curso->getDiasSemana(), $curso->getDataTermino());
         }
-
-
-        public function testeInsert($res){
-            if($res != TRUE)
-                echo 'falha na operação';
+         
+        public function deletaCurso($nome) {
+            $this->_ins.= " ('$nome')";
+            $res= mysql_query($this->ins);
+            if($_res != TRUE){
+                echo "falha ao cadastrar";
+                }
+                else{
+                    echo "Cadastrado com sucesso!";
+                
+            }
+            return $_res;            
         }
-               
+        
+        public function deletaCurso2($curso) {
+            return $this->deletaCurso($curso->getNome());
+        }
     }
 ?>
