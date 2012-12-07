5ab0d509eea6fe36e836632422ae6ff01fe92b9d
brunocesar.bsi <brunocesar.bsi@gmail.com>
2012-12-07 16:26:56 +0000

    oi oi oi

--- acao/controle/cCadastraFuncionario.php
+++ acao/controle/cCadastraFuncionario.php
@@ -1,35 +1,24 @@
 <?php
 
+include_once 'cFuncoes.php';
+echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
 
-    include_once 'cFuncoes.php';
-    echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
+include_once '../modelo/Modelo.php';
+include_once '../bd/LoginDAO.php';
 
-    include_once '../modelo/Modelo.php';
-    include_once '../bd/LoginDAO.php';
-    
-    
-    $usuario= $_POST['usuario'];
-    $senha= $_POST['senha'];
-    $nivel= $_POST['nivel'];
-    
-        
-    $login = new Login($usuario, $senha, $nivel);
-    $loginDAO = new LoginDAO();
-    $loginDAO->cadastraLogin($login);
-    $retorno = $loginDAO->cadastraLogin($_res);
-    
-    if($retorno != TRUE){          
-                echo "<script>alert('Falha ao cadastrar!');</script>";
-                header("Location:../visao/vCadastroFuncionario.php");
-                
-                }
-                else{                    
-                    echo "<script>alert('Cadastrado com sucesso!');</script>";
-                    header("Location:../visao/vAtentente.php");
-        
-    }
-    
-    
-    
-    
+
+$usuario = $_POST['usuario'];
+$senha = $_POST['senha'];
+$nivel = $_POST['nivel'];
+
+
+$login = new Login($usuario, $senha, $nivel);
+$loginDAO = new LoginDAO();
+$retorno = $loginDAO->cadastraLogin($login);
+
+if ($retorno != TRUE) {
+    echo "<script>alert('Falha ao cadastrar!');location.href='../visao/vCadastroFuncionario.php';</script>";
+} else {
+    echo "<script>alert('Cadastrado com sucesso!');location.href='../visao/vAtentente.php';</script>";
+}
 ?>
