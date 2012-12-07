b133f76a95d2a49bd53f1e6a93f8f7f26f79f1f8
rafael.vmendes2@gmail.com <rafael.vmendes2@gmail.com@a9a1e803-4b22-2b6a-56cc-01784704d63b>
2012-12-07 21:02:26 +0000

    Corrigido um bug em autocompletar em vIncluirPessoaCurso.php (ainda falta um scroll); arrumado os botões em montaTabela.php

--- trunk/acao/controle/montaTabela.php
+++ trunk/acao/controle/montaTabela.php
@@ -1,145 +1,147 @@
 <script type="text/javascript">
     
-   $(".__idFamilia").click(function(){
+    $(".__idFamilia").click(function(){
             
-       var id = $(this).attr('id');
+        var id = $(this).attr('id');
        
-      var novaURL = "http://www.codigofonte.com.br/";
-    $(window.document.location).attr('href',"../visao/vCadastroPessoa.php");
+        var novaURL = "http://www.codigofonte.com.br/";
+        $(window.document.location).attr('href',"../visao/vCadastroPessoa.php");
             
-   })
+    })
     
 </script>
 <?php
-
 include_once '../bd/DBConnection.php';
 include_once '../bd/FamiliaDAO.php';
 include_once '../bd/PessoaDAO.php';
 DataBase::createConection();
 /*
-$id= $_REQUEST['id'];
-$titular= $_REQUEST['titular'];
-$endereco= $_REQUEST['endereco'];*/
-$html='<form action="../visao/vCadastroPessoaComFamilia.php" method="post">';
-$query= $_REQUEST['query'];
-$fD= new FamiliaDAO;
-    $pD= new PessoaDAO;
+  $id= $_REQUEST['id'];
+  $titular= $_REQUEST['titular'];
+  $endereco= $_REQUEST['endereco']; */
+$html = '<form action="../visao/vCadastroPessoaComFamilia.php" method="post">';
+$query = $_REQUEST['query'];
+$fD = new FamiliaDAO;
+$pD = new PessoaDAO;
 
-if(!$query){//busca vazia retorno irrelevante 
-    $resultado= $fD->buscaFamilia();    
-    $html  .= "<table border= 1 width=500px>";    
+if (!$query) {//busca vazia retorno irrelevante 
+    $resultado = $fD->buscaFamilia();
+    $html .= "<table border= 1 width=500px>";
     $html .= "<th>id</th>";
     $html .= "<th>nome titular</th>";
     $html .= "<th>endereço</th>";
-    $html .= "<th>seleção</th>";   
-    while ($a= mysql_fetch_assoc($resultado)){        
+    $html .= "<th>seleção</th>";
+    while ($a = mysql_fetch_assoc($resultado)) {
         $html .= '<tr>';
-        $html .= '<td>'.$a['id_familia'].'</td>';
-        $html .= '<td>'.$pD->buscaPessoabyFamilia($a['id_familia']).'</td>';
-        $html .= '<td>'.$a['logradouro'].' - '.$a['numero'].'</td>';
-        $html .= '<td>'.'<input type="submit" name= "id_familia" value="'.$a['id_familia'].'" img src="../imagens/next.png">'.'</td>';
+        $html .= '<td>' . $a['id_familia'] . '</td>';
+        $html .= '<td>' . $pD->buscaPessoabyFamilia($a['id_familia']) . '</td>';
+        $html .= '<td>' . $a['logradouro'] . ' - ' . $a['numero'] . '</td>';
+        $html .= '<center>' . '<td>' . '<input src="../imagens/next.png" type="image" name= "id_familia" value="' . $a['id_familia'] . '">' . '</td>' . '</center>';
+        //$html .= '<td>'.'<input type="submit" name= "id_familia" value="'.$a['id_familia'].'" img src="../imagens/next.png">'.'</td>';
         //$html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
-        $html .= '</tr>';      
-    } 
-}
-
-elseif (is_numeric($query)) { //provavel q a pesquisa seja por ID ou numero da casa, seguido por resultados sem relevancia  
-    $resultadoId= $fD->buscaFamiliaById($query);
-    $html  .= "<table border= 1 width=500px>";    
+        $html .= '</tr>';
+    }
+} elseif (is_numeric($query)) { //provavel q a pesquisa seja por ID ou numero da casa, seguido por resultados sem relevancia  
+    $resultadoId = $fD->buscaFamiliaById($query);
+    $html .= "<table border= 1 width=500px>";
     $html .= "<th>id</th>";
     $html .= "<th>nome titular</th>";
     $html .= "<th>endereço</th>";
     $html .= "<th>seleção</th>";
     //busca a familia cujo ID seja igual ao numero digitado
-     while ($a= mysql_fetch_assoc($resultadoId)){        
+    while ($a = mysql_fetch_assoc($resultadoId)) {
         $html .= '<tr>';
-        $html .= '<td>'.$a['id_familia'].'</td>';
-        $html .= '<td>'.$pD->buscaPessoabyFamilia($a['id_familia']).'</td>';
-        $html .= '<td>'.$a['logradouro'].' - '.$a['numero'].'</td>';
-        $html .= '<td>'.'<input type="submit" name= "id_familia" value="'.$a['id_familia'].'" img src="../imagens/next.png">'.'</td>';
+        $html .= '<td>' . $a['id_familia'] . '</td>';
+        $html .= '<td>' . $pD->buscaPessoabyFamilia($a['id_familia']) . '</td>';
+        $html .= '<td>' . $a['logradouro'] . ' - ' . $a['numero'] . '</td>';
+        $html .= '<center>' . '<td>' . '<input src="../imagens/next.png" type="image" name= "id_familia" value="' . $a['id_familia'] . '">' . '</td>' . '</center>';
+        //$html .= '<td>' . '<input type="submit" name= "id_familia" value="' . $a['id_familia'] . '" img src="../imagens/next.png">' . '</td>';
         //$html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
-        $html .= '</tr>';      
+        $html .= '</tr>';
     }
-    
-    //if(mysql_num_rows($resultadoId)){
-    $resultadoNum= $fD->buscaFamiliaByNumero($query);//busca de numeros de casa
-    while ($a= mysql_fetch_assoc($resultadoNum)){
-        $html .= '<tr>';
-        $html .= '<td>'.$a['id_familia'].'</td>';
-        $html .= '<td>'.$pD->buscaPessoabyFamilia($a['id_familia']).'</td>';
-        $html .= '<td>'.$a['logradouro'].' - '.$a['numero'].'</td>';
-        $html .= '<td>'.'<input type="submit" name= "id_familia" value="'.$a['id_familia'].'" img src="../imagens/next.png">'.'</td>';
-        //$html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
-        $html .= '</tr>'; 
-    }
-    
-    //concatena com resultados menos relevantes, pois pode ser que a primeira resposta naum seja importante
-    $resultadoId2= $fD->buscaFamiliaExceptId($query);
-    while ($a= mysql_fetch_assoc($resultadoId2)){        
-        $html .= '<tr>';
-        $html .= '<td>'.$a['id_familia'].'</td>';
-        $html .= '<td>'.$pD->buscaPessoabyFamilia($a['id_familia']).'</td>';
-        $html .= '<td>'.$a['logradouro'].' - '.$a['numero'].'</td>';
-        $html .= '<td>'.'<input type="submit" name= "id_familia" value="'.$a['id_familia'].'" img src="../imagens/next.png">'.'</td>';
-        //$html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
-        $html .= '</tr>';      
-    }    
-}
 
- else {//a pesquisa é texto, pode ser um endereço ou titular
-   $tamanhoPesquisa= strlen($query);
-   $radical= substr($query, 1, $tamanhoPesquisa-2);
-   $resultado3= $pD->buscaPessoaTitular($radical);
-   $html  .= "<table border= 1 width=500px>";    
+    //if(mysql_num_rows($resultadoId)){
+    $resultadoNum = $fD->buscaFamiliaByNumero($query); //busca de numeros de casa
+    while ($a = mysql_fetch_assoc($resultadoNum)) {
+        $html .= '<tr>';
+        $html .= '<td>' . $a['id_familia'] . '</td>';
+        $html .= '<td>' . $pD->buscaPessoabyFamilia($a['id_familia']) . '</td>';
+        $html .= '<td>' . $a['logradouro'] . ' - ' . $a['numero'] . '</td>';
+        $html .= '<center>' . '<td>' . '<input src="../imagens/next.png" type="image" name= "id_familia" value="' . $a['id_familia'] . '">' . '</td>' . '</center>';
+        //$html .= '<td>' . '<input type="submit" name= "id_familia" value="' . $a['id_familia'] . '" img src="../imagens/next.png">' . '</td>';
+        //$html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
+        $html .= '</tr>';
+    }
+
+    //concatena com resultados menos relevantes, pois pode ser que a primeira resposta naum seja importante
+    $resultadoId2 = $fD->buscaFamiliaExceptId($query);
+    while ($a = mysql_fetch_assoc($resultadoId2)) {
+        $html .= '<tr>';
+        $html .= '<td>' . $a['id_familia'] . '</td>';
+        $html .= '<td>' . $pD->buscaPessoabyFamilia($a['id_familia']) . '</td>';
+        $html .= '<td>' . $a['logradouro'] . ' - ' . $a['numero'] . '</td>';
+        $html .= '<center>' . '<td>' . '<input src="../imagens/next.png" type="image" name= "id_familia" value="' . $a['id_familia'] . '">' . '</td>' . '</center>';
+        //$html .= '<td>' . '<input type="submit" name= "id_familia" value="' . $a['id_familia'] . '" img src="../imagens/next.png">' . '</td>';
+        //$html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
+        $html .= '</tr>';
+    }
+} else {//a pesquisa é texto, pode ser um endereço ou titular
+    $tamanhoPesquisa = strlen($query);
+    $radical = substr($query, 1, $tamanhoPesquisa - 2);
+    $resultado3 = $pD->buscaPessoaTitular($radical);
+    $html .= "<table border= 1 width=500px>";
     $html .= "<th>id</th>";
     $html .= "<th>nome titular</th>";
     $html .= "<th>endereço</th>";
     $html .= "<th>seleção</th>";
-    while ($a= mysql_fetch_assoc($resultado3)){        
+    while ($a = mysql_fetch_assoc($resultado3)) {
         $html .= '<tr>';
-        $html .= '<td>'.$a['id_familia'].'</td>';
-        $html .= '<td>'.$a['nome'].'</td>';
-        $html .= '<td>'.$fD->buscaLogradouro($a['id_familia']) .' - '.$fD->buscaNumero($a['id_familia']).'</td>';
-        $html .= '<td>'.'<input type="submit" name= "id_familia" value="'.$a['id_familia'].'" img src="../imagens/next.png">'.'</td>';
+        $html .= '<td>' . $a['id_familia'] . '</td>';
+        $html .= '<td>' . $a['nome'] . '</td>';
+        $html .= '<td>' . $fD->buscaLogradouro($a['id_familia']) . ' - ' . $fD->buscaNumero($a['id_familia']) . '</td>';
+        $html .= '<center>' . '<td>' . '<input src="../imagens/next.png" type="image" name= "id_familia" value="' . $a['id_familia'] . '">' . '</td>' . '</center>';
+        //$html .= '<td>' . '<input type="submit" name= "id_familia" value="' . $a['id_familia'] . '" img src="../imagens/next.png">' . '</td>';
         //$html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
-        $html .= '</tr>';  
+        $html .= '</tr>';
     }
-    
-    $resultado4= $fD->buscaFamiliabyLogradouro($query);
-    while ($a= mysql_fetch_assoc($resultado4)){        
+
+    $resultado4 = $fD->buscaFamiliabyLogradouro($query);
+    while ($a = mysql_fetch_assoc($resultado4)) {
         $html .= '<tr>';
-        $html .= '<td>'.$a['id_familia'].'</td>';
-        $html .= '<td>'.$pD->buscaPessoabyFamilia($a['id_familia']).'</td>';
-        $html .= '<td>'.$a['logradouro'].' - '.$a['numero'].'</td>';
-        $html .= '<td>'.'<input type="submit" name= "id_familia" value="'.$a['id_familia'].'" img src="../imagens/next.png">'.'</td>';
+        $html .= '<td>' . $a['id_familia'] . '</td>';
+        $html .= '<td>' . $pD->buscaPessoabyFamilia($a['id_familia']) . '</td>';
+        $html .= '<td>' . $a['logradouro'] . ' - ' . $a['numero'] . '</td>';
+        $html .= '<center>' . '<td>' . '<input src="../imagens/next.png" type="image" name= "id_familia" value="' . $a['id_familia'] . '">' . '</td>' . '</center>';
+        //$html .= '<td>' . '<input type="submit" name= "id_familia" value="' . $a['id_familia'] . '" img src="../imagens/next.png">' . '</td>';
         //$html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
-        $html .= '</tr>';  
+        $html .= '</tr>';
     }
     //mostrar resultados menos relevantes
-    $resultado5= $fD->buscaFamiliaExceptLogradouro($query);
-    
-    while ($a1= mysql_fetch_assoc($resultado5)){        
+    $resultado5 = $fD->buscaFamiliaExceptLogradouro($query);
+
+    while ($a1 = mysql_fetch_assoc($resultado5)) {
         $html .= '<tr>';
-        $html .= '<td>'.$a1['id_familia'].'</td>';
-        $html .= '<td>'.$pD->buscaPessoabyFamilia($a1['id_familia']).'</td>';
-        $html .= '<td>'.$a1['logradouro'].' - '.$a1['numero'].'</td>';
-        $html .= '<td>'.'<input type="submit" name= "id_familia" value="'.$a1['id_familia'].'" img src="../imagens/next.png">'.'</td>';
+        $html .= '<td>' . $a1['id_familia'] . '</td>';
+        $html .= '<td>' . $pD->buscaPessoabyFamilia($a1['id_familia']) . '</td>';
+        $html .= '<td>' . $a1['logradouro'] . ' - ' . $a1['numero'] . '</td>';
+        $html .= '<center>' . '<td>' . '<input src="../imagens/next.png" type="image" name= "id_familia" value="' . $a['id_familia'] . '">' . '</td>' . '</center>';
+        //$html .= '<td>' . '<input type="submit" name= "id_familia" value="' . $a1['id_familia'] . '" img src="../imagens/next.png">' . '</td>';
         //$html .= '<td>'.'<img src="../imagens/next.png" class="__idFamilia" id="'.$a['id_familia'].'">'.'</td>';
-        $html .= '</tr>';        
+        $html .= '</tr>';
     }
 }
- $html  .= "</table>";
- $html .= "</form> ";
-      echo $html;
+$html .= "</table>";
+$html .= "</form> ";
+echo $html;
 
 /*
-$res= Pesquisa::buscaGenerica("pesssoa", "nome", "Renato");
+  $res= Pesquisa::buscaGenerica("pesssoa", "nome", "Renato");
 
-//echo "<table border= 1>";
-//echo "<tr bgcolor= '#f0f0f0'>";
-for($_i= 0; $_i< mysql_num_fields($res); $_i++){
-    echo mysql_field_name($res, $_i);//. "</td>";
-}
-//echo '</tr>';
-*/
+  //echo "<table border= 1>";
+  //echo "<tr bgcolor= '#f0f0f0'>";
+  for($_i= 0; $_i< mysql_num_fields($res); $_i++){
+  echo mysql_field_name($res, $_i);//. "</td>";
+  }
+  //echo '</tr>';
+ */
 ?>
