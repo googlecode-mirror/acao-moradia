396330c2ae4e288b355c4e7ae4ac4afb2c583229
renato.rsufu <renato.rsufu@gmail.com>
2012-12-11 02:59:00 +0000

    correção de alguns arquivos, melhoramento de metodos de pesquisa e cadastro de familias, terminando update e delete..

---
+++ acao/controle/cControleFamilia.php
@@ -0,0 +1,25 @@
+<?php
+
+    $query= $_POST['id_familia'];
+    $tamanhoPesquisa = strlen($query);    
+    $parametro = substr($query, 1, $tamanhoPesquisa - 1);//detecta q operação deve-se fazer: (a= add pessoa à familia, e= editar familia e d= deletar familia)
+    $id_familia = substr($query, 0, $tamanhoPesquisa-1);
+    echo $query.'</br>';
+    echo $parametro.'</br>';
+    echo $id_familia;
+    
+    if($parametro === 'a'){
+        session_start();
+        $_SESSION['id_familia']= $id_familia;
+        header("Location: ../visao/vCadastroPessoaComFamilia2.php");
+    }
+     elseif($parametro === 'e'){
+        session_start();
+        $_SESSION['id_familia']= $id_familia;
+        header("Location: ../visao/vCadastroPessoaComFamilia2.php");
+    }
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
