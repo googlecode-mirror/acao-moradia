7d8325836712e955276c8fd3f662ed166a3bb6d4
lpfw82 <lpfw82@gmail.com>
2013-03-15 20:01:21 +0000

    Alterando o rodapé

--- acao/visao/vLayoutFooter.php
+++ acao/visao/vLayoutFooter.php
@@ -1,9 +1,36 @@
-<div class="wrap">
-    <a href="http://www.petsi.facom.ufu.br" target="_blank">
+<div class="wrap">    
         <div class="footer">
-            <div class="foot">                          	
-                <div class="copyright"></div>
-            </div>
+            <div class="foot">                          	                
+                <div class="copyright">                                        
+                </div>
+                <br />
+                
+                <div style="font-size: 13px;">
+                    <a href="http://http://acaomoradia.org.br/" target="_blank">
+                    Ação Moradia:
+                    <br />
+                    Rua Canoas nº181, Morumbi | Uberlândia - MG 
+                    <br />
+                    Cep: 38407-291 Brasil | Fone: (34)3226-6558
+                    <br />
+                    Email: comunicacao@acaomoradia.org.br
+                    </a>
+                </div>                
+                                
+                <br />
+                <div style="font-size: 13px;margin-left: 700px;margin-top: -91px;">
+                    <a href="http://www.petsi.facom.ufu.br" target="_blank">
+                    PET-Sistemas de Informação:
+                    <br />
+                    Av. João Naves de Ávila, nº2121, Santa Mônica | Uberlândia - MG                                                 
+                    <br />
+                    CEP 38408-100  Brasil
+                    <br />
+                    Email: petsi-ufu@googlegroups.com                        
+                    </a>
+                </div>
+
+            </div>            
         </div>
-    </a>
+    </a>    
 </div>
