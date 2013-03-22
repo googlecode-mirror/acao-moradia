05b29cdd028f80f135a059aa4b19192a26d291ad
lpfw82 <lpfw82@gmail.com>
2013-03-22 14:13:17 +0000

    Documentos de ajuda: agora, o documento de ajuda está baixando dados direto do Google code no link https://acao-moradia.googlecode.com/files/ajuda.pdf, portanto, é possível alterar o guia de usuário (ajuda.odt) em qualquer computador e atualizá-lo no google code, mas NÃO pode mudar o nome dele(ajuda.pdf).

--- acao/visao/vLayoutBody.php
+++ acao/visao/vLayoutBody.php
@@ -13,7 +13,7 @@
                 <div class="menu_bts">
                     <a href="../visao/vAtendente.php" target="_parent"><img src="../imagens/menu_home.png" width="94" height="73" /></a>
                     <a href="" onClick="alert('Desenvolvido pelo PET - Sistemas de Informação - UFU\n\nUniversidade Federal de Uberlândia - 2012\n\nhttp://www.petsi.facom.ufu.br')" target="_parent"><img src="../imagens/menu_sobre.png" width="81" height="73" /></a>
-                    <a href="../pdf/ajuda.pdf" target="_blank"><img src="../imagens/menu_ajuda.png" width="76" height="73" /></a>
+                    <a href="https://acao-moradia.googlecode.com/files/ajuda.pdf" target="_blank"><img src="../imagens/menu_ajuda.png" width="76" height="73" /></a>
                     <a href="../controle/cLogout.php" target="_parent"><img src="../imagens/menu_logout.png" width="62" height="73" /></a>
                 </div>
             </div>
